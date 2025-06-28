124acb2c69dd494fc8732aa3eb040f4f7160e425
Angell Li <angell.l@zkm.io>
2025-06-28 19:45:46 +0800

    add debug log for all instruction and register states at block head

--- crates/core/executor/src/executor.rs
+++ crates/core/executor/src/executor.rs
@@ -1039,6 +1039,7 @@ impl<'a> Executor<'a> {
         let mut hi_or_prev_a = None;
         let mut syscall_code = 0u32;
 
+        let print_registers = self.state.next_is_delayslot;
         self.state.next_is_delayslot = false;
 
         if self.executor_mode == ExecutorMode::Trace {
@@ -1283,7 +1284,17 @@ impl<'a> Executor<'a> {
                 return Err(ExecutionError::UnsupportedInstruction(instruction.op_c));
             }
         }
-
+        log::debug!(
+            "Executing instruction: {:?} at PC: {:08X}, Next PC: {:08X}, Next Next PC: {:08X}， a: {:08X}, b: {:08X}, c: {:08X}, hi_or_prev_a: {:?}",
+            instruction,
+            pc,
+            next_pc,
+            next_next_pc,
+            a,
+            b,
+            c,
+            hi_or_prev_a
+        );
         // Emit the CPU event for this cycle.
         if self.executor_mode == ExecutorMode::Trace {
             self.emit_events(
@@ -1305,6 +1316,18 @@ impl<'a> Executor<'a> {
         // Update the program counter.
         self.state.pc = next_pc;
         self.state.next_pc = next_next_pc;
+
+        if print_registers {
+            let mut regs =[0u32; 34];
+            for reg in 0..34 {
+                regs[reg] = self.register((reg as u8).into());
+            }
+
+            log::debug!(
+                "PC: {:08X}, Next PC: {:08X} regs {:08X?}\n",
+                self.state.pc, self.state.next_pc, regs
+            );
+        }
 
         // Update the clk to the next cycle.
         self.state.clk += 5;
